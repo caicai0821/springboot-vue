9f29fdbcca401f6c98b18d54c2ab8491b329e0cd
caicai0821 <736814997@qq.com>
2023-07-06 18:08:32 +0800

    测试

---
+++ vue/src/App.vue
@@ -0,0 +1,14 @@
+<!--
+ * @Author       : 蔡诗涵
+ * @Date         : 2023-07-06 10:31:52
+ * @LastEditTime : 2023-07-06 17:54:25
+ * @Description  : 
+ * 
+-->
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style lang="less"></style>
---
+++ vue/src/main.js
@@ -0,0 +1,21 @@
+/*
+ * @Author       : 蔡诗涵
+ * @Date         : 2023-07-06 10:31:52
+ * @LastEditTime : 2023-07-06 16:40:24
+ * @Description  :
+ *
+ */
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI);
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: (h) => h(App),
+}).$mount('#app')
