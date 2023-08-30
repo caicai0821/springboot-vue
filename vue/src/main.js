0cd9653e2e6a081f75dd9ed1bf735b3258bff754
caicai0821 <736814997@qq.com>
2023-08-30 15:54:05 +0800

    重新初始化

--- vue/src/main.js
+++ vue/src/main.js
@@ -10,8 +10,9 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import '@/assets/comment.css'
 
-Vue.use(ElementUI);
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,3 +1,10 @@
+/*
+ * @Author       : 蔡诗涵
+ * @Date         : 2023-07-06 10:31:52
+ * @LastEditTime : 2023-08-30 15:53:36
+ * @Description  :
+ *
+ */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
@@ -8,22 +15,14 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes,
 })
 
 export default router
